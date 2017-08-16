2629b2f0a25c4f5b078a6deb1f8a1d0fda079992
Ankur Patel <ankur.patel@ymail.com>
2017-08-16 18:11:33 -0400

    Refactoring

--- server/watson-discovery-service.js
+++ server/watson-discovery-service.js
@@ -5,7 +5,6 @@ const discovery = new DiscoveryV1({
   version_date: '2017-08-01'
 });
 
-discovery.getEnvironments = Promise.promisify(discovery.getEnvironments);
 discovery.query = Promise.promisify(discovery.query);
 
 module.exports = discovery;
